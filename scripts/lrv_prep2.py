6a4da92ad347c112bbbb5b5b655861e41f2b5c7d
drdhaval2785 <drdhaval2785@gmail.com>
2022-09-20 18:25:55 +0530

    lrv2 step2

--- scripts/lrv_prep2.py
+++ scripts/lrv_prep2.py
@@ -17,13 +17,24 @@ if __name__ == "__main__":
 	fin = codecs.open(filein, 'r', 'utf-8')
 	fout = codecs.open(fileout, 'w', 'utf-8')
 	reader = csv.reader(fin, delimiter='\t')
+	h1 = ''
 	for row in reader:
 		lnum = row[0]
 		pc = row[1]
 		k2 = row[2]
+		if k2.startswith('<p>'):
+			clean_k2 = re.sub('<p>[ ]*', '', k2)
+			h1 = clean_k2
+		elif k2.startswith('<b>'):
+			clean_k2 = h1 + re.sub('<b>[ ]*', '', k2)
+		elif k2 == '':
+			clean_k2 = clean_k2
+		else:
+			print(k2)
+		prevhw = k2
 		grammar = row[3]
 		entry = row[4].rstrip()
-		fout.write('<L>' + lnum + '<pc>' + pc + '<k1>' + k2 + '<k2>' + k2 + '\n')
+		fout.write('<L>' + lnum + '<pc>' + pc + '<k1>' + clean_k2 + '<k2>' + clean_k2 + '\n')
 		fout.write(lnum + '\t' + pc + '\t' + k2 + '\t' + grammar + '\t' + entry + '\n')
 	fin.close()
 	fout.close()

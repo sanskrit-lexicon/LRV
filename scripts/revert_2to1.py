efb405e2b5a774ac542f42d4055dcd97a9f9d033
drdhaval2785 <drdhaval2785@gmail.com>
2022-09-21 22:26:06 +0530

    reversion2to1 step2

--- scripts/revert_2to1.py
+++ scripts/revert_2to1.py
@@ -24,6 +24,9 @@ if __name__ == "__main__":
 	fin = codecs.open(filein, 'r', 'utf-8')
 	fout = codecs.open(fileout, 'w', 'utf-8')
 	counter = 0
+	prevpc = ''
+	prevk1 = ''
+	prevk2 = ''
 	for lin in fin:
 		if counter == 500:
 			exit(0)
@@ -36,8 +39,8 @@ if __name__ == "__main__":
 			k2 = m.group(4)
 			if '-' in k2:
 				# अंश-अंशि -> -अंशि
-				k2secondpart = k2.replace(k1 + '-', '-')
-				tabclass = ('<b> ', '<b> ', '<+> ')
+				k2secondpart = k2.split('-')[1]
+				tabclass = ('<b> -', '<b> ', '<+> ')
 			else:
 				k2secondpart = k2
 				tabclass = ('<p> ', '<p> ', '<p> ')
@@ -46,7 +49,15 @@ if __name__ == "__main__":
 		elif lin.startswith('<LEND>'):
 			# 00005	001-02	<p> अऋणिन्	<p> अऋणिन्	<p> अऋणिन्	#-a. (f. नी)	$--Free from debt.	18
 			# lnum	pc	k2secondpart	k2	k1	gram	entry	enlen
-			fout.write(lnum + '\t' + pc + '\t' + tabclass[0] + k2secondpart + '\t' + tabclass[1] + k2 + '\t' + tabclass[2] + k1 + '\t#-' + gram + '\t$--' + entry + '\t' + enlen + '\n')             
+			if pc == prevpc and k2 == prevk2:
+				fout.write(lnum + '\t' + '' + '\t' + '' + '\t' + '' + '\t' + '' + '\t#-' + gram + '\t$--' + entry + '\t' + enlen + '\n')             
+			elif pc == prevpc and k2 != prevk2:
+				fout.write(lnum + '\t' + '' + '\t' + tabclass[0] + k2secondpart + '\t' + tabclass[1] + k2 + '\t' + tabclass[2] + k1 + '\t#-' + gram + '\t$--' + entry + '\t' + enlen + '\n')             
+			else:
+				fout.write(lnum + '\t' + pc + '\t' + tabclass[0] + k2secondpart + '\t' + tabclass[1] + k2 + '\t' + tabclass[2] + k1 + '\t#-' + gram + '\t$--' + entry + '\t' + enlen + '\n')             
+			prevpc = pc
+			prevk2 = k2
+			prevk1 = k1
 		else:
 			(k2, gram, entry) = lin.split('\t')
 			entry = entry.rstrip()

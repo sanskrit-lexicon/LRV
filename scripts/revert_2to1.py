35ed1f79a1eb24b9b698e5bf4efc12c8f21962f9
drdhaval2785 <drdhaval2785@gmail.com>
2022-09-21 23:19:35 +0530

    revert_2to1 step2

--- scripts/revert_2to1.py
+++ scripts/revert_2to1.py
@@ -16,14 +16,12 @@ import sys
 import csv
 import re
 import codecs
+from collections import defaultdict
 
 
-if __name__ == "__main__":
-	filein = sys.argv[1]
-	fileout = sys.argv[2]
+def first_run(filein, fileout):
 	fin = codecs.open(filein, 'r', 'utf-8')
 	fout = codecs.open(fileout, 'w', 'utf-8')
-	counter = 0
 	prevpc = ''
 	prevk1 = ''
 	prevk2 = ''
@@ -60,4 +58,60 @@ if __name__ == "__main__":
 			(k2, gram, entry) = lin.split('\t')
 			entry = entry.rstrip()
 			enlen = str(len(entry) + 3)
-		counter += 1
+
+
+def second_run(fileout1, fileout2):
+	# 00011		<b> -हर	<b> अंश-हर	<+> अंशहर	#-a.	$--a sharer, पिंडदोंशहरश्चैषां पूर्वाभावे परः परः /Yaj./ii.132.	63
+	# 00011.1		<b> -हारिन्	<b> अंश-हारिन्	<+> अंशहारिन्	#-a.	$--a sharer, पिंडदोंशहरश्चैषां पूर्वाभावे परः परः /Yaj./ii.132.	63
+	# ---
+	# 00011		<b> -हर, -हारिन्	<b> अंश-हर, अंश-हारिन्	<+> अंशहर, अंशहारिन्	#-a.	$--a sharer, पिंडदोंशहरश्चैषां पूर्वाभावे परः परः /Yaj./ii.132.	63
+	fin1 = codecs.open(fileout1, 'r', 'utf-8')
+	data = fin1.readlines()
+	fin1.close()
+	fout = codecs.open(fileout2, 'w', 'utf-8')
+	result = defaultdict(list)
+	for lin in data:
+		lin = lin.rstrip()
+		split = lin.split('\t')
+		lnum = split[0]
+		pc = split[1]
+		k2secondpart = split[2]
+		k2 = split[3]
+		k1 = split[4]
+		gram = split[5]
+		entry = split[6]
+		charcount = split[7]
+		if not '.' in lnum:
+			result[lnum].append((lnum, pc, k2secondpart, k2, k1, gram, entry, charcount))
+			lnum_bare = lnum
+		else:
+			lnum_bare = lnum.split('.')[0]
+			result[lnum_bare].append((lnum, pc, k2secondpart, k2, k1, gram, entry, charcount))
+	for (key, value) in result.items():
+		lnum = key
+		pc = value[0][1]
+		k2secondpart = ', '.join([x[2] for x in value])
+		k2secondpart = k2secondpart.replace(', <b>', ',')
+		k2 = ', '.join([x[3] for x in value])
+		k2 = k2.replace(', <b>', ',')
+		k1 = ', '.join([x[4] for x in value])
+		k1 = k1.replace(', <+>', ',')
+		gram = value[0][5]
+		entry = value[0][6]
+		enlen = value[0][7]
+		if len(value) == 1:
+			fout.write(lnum + '\t' + pc + '\t' + k2secondpart + '\t' + k2 + '\t' + k1 + '\t' + gram + '\t' + entry + '\t' + enlen + '\n')
+		else:
+			fout.write(lnum + '\t' + pc + '\t' + '<b> ' + k2secondpart + '\t' + '<b> ' + k2 + '\t' + '<+> ' + k1 + '\t' + gram + '\t' + entry + '\t' + enlen + '\n')
+		print(key, len(result[key]))
+	#print(result['00011'])
+
+		
+	fout.close()
+
+
+if __name__ == "__main__":
+	filein = sys.argv[1]
+	fileout = sys.argv[2]
+	first_run(filein, fileout)
+	second_run(fileout, 'temp.txt')

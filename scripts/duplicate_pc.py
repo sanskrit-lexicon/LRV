b346dae7cc0a2fb7a89419a09e236623c5a1aae1
drdhaval2785 <drdhaval2785@gmail.com>
2022-10-04 11:02:58 +0530

    code to find duplicate pc

---
+++ scripts/duplicate_pc.py
@@ -0,0 +1,15 @@
+import codecs
+import csv
+
+if __name__ == "__main__":
+	fin = codecs.open('../interim/lrv_0.txt', 'r', 'utf-8')
+	reader = csv.reader(fin, delimiter='\t')
+	result = set()
+	for row in reader:
+		pc = row[1]
+		if pc != '':
+			if pc in result:
+				print(pc)
+				print('\t'.join(row))
+			result.add(pc)
+		

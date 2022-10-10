095cc4daa35865889560fee33c278bca50e8bcfc
drdhaval2785 <drdhaval2785@gmail.com>
2022-10-10 13:48:48 +0530

    lrv_5 started to handle alternate headwords of #12

--- scripts/lrv_prep5.py
+++ scripts/lrv_prep5.py
@@ -6,6 +6,7 @@ Usage - python3 lrv_prep5.py ../interim/lrv_4.txt ../interim/lrv_5.txt
 import sys
 import re
 import codecs
+import json
 import parseheadline
 
 def prepare_metaline(meta):
@@ -17,6 +18,8 @@ def prepare_metaline(meta):
 if __name__ == "__main__":
 	filein = sys.argv[1]
 	fileout = sys.argv[2]
+	changefile = '../interim/change_metalines.txt'
+	cfout = codecs.open(changefile, 'w', 'utf-8')
 	fin = codecs.open(filein, 'r', 'utf-8')
 	fout = codecs.open(fileout, 'w', 'utf-8')
 	changed_entries = {}
@@ -25,10 +28,14 @@ if __name__ == "__main__":
 		if lin.startswith('<L>'):
 			meta = parseheadline.parseheadline(lin)
 			if '(' in meta['k1']:
+				lnum = meta['L']
 				meta['k1'] = re.sub('\(.*?\)', '', meta['k1'])
 				metaline = prepare_metaline(meta)
 				fout.write(metaline + '\n')
+				changed_entries[lnum] = lin
 			else:
 				fout.write(lin + '\n')
 		else:
 			fout.write(lin + '\n')
+	json.dump(changed_entries, cfout, indent='\t')
+	

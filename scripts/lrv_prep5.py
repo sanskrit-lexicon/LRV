7f380b0b09495b46d1fdcb702434fe0231ebc2d0
drdhaval2785 <drdhaval2785@gmail.com>
2022-10-10 13:21:44 +0530

    trial for #12

---
+++ scripts/lrv_prep5.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Convert key1 with brackets to without brackets.
+
+Usage - python3 lrv_prep5.py ../interim/lrv_4.txt ../interim/lrv_5.txt
+"""
+import sys
+import re
+import codecs
+import parseheadline
+
+def prepare_metaline(meta):
+	result = ''
+	for key, value in meta.items():
+		result += '<' + key + '>' + value
+	return result
+
+if __name__ == "__main__":
+	filein = sys.argv[1]
+	fileout = sys.argv[2]
+	fin = codecs.open(filein, 'r', 'utf-8')
+	fout = codecs.open(fileout, 'w', 'utf-8')
+	changed_entries = {}
+	for lin in fin:
+		lin = lin.rstrip('\n')
+		if lin.startswith('<L>'):
+			meta = parseheadline.parseheadline(lin)
+			if '(' in meta['k1']:
+				meta['k1'] = re.sub('\(.*?\)', '', meta['k1'])
+				metaline = prepare_metaline(meta)
+				fout.write(metaline + '\n')
+			else:
+				fout.write(lin + '\n')
+		else:
+			fout.write(lin + '\n')
